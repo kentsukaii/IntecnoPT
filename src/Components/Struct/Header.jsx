9df89a4d6b7786d82d0a37a58ac5e6e0f7a7095a
pialo3434 <pccosta2001@gmail.com>
2023-11-25 20:18:43 +0000

    Header fully fixed + js

--- src/Components/Struct/Header.jsx
+++ src/Components/Struct/Header.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import logotype from '../../assets/logotype.png';
 import maguire from '../../assets/godmaguire.jpg';
+import './Header.css';
 import { Link } from 'react-router-dom';
 import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol } from 'mdbreact';
 
@@ -12,60 +13,62 @@ function Header() {
     return (
         // JSX that represents the header element
 
-<header className="header">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{height: '80px'}}>
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">
-                <img src={logotype} alt="Your Brand" style={{height: '40px', width: '180px'}} />
-            </a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                        <a className="nav-link" href="/dashboard">Dashboard</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/team">Team</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/projects">Projects</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="/login">Login</a>
-                    </li>
-                </ul>
-                <div className="d-flex align-items-center">
-                    <div className="form-check form-switch">
-                        <input className="form-check-input" type="checkbox" id="darkModeSwitch" />
-                        <label className="form-check-label" htmlFor="darkModeSwitch">
-                            <i className="fas fa-sun"></i>
-                            <i className="fas fa-moon"></i>
-                        </label>
-                    </div>
-                    <a className="nav-link position-relative" href="/cart">
-                        <i className="fas fa-shopping-cart fa-lg"></i>
-                        <span className="badge bg-danger position-absolute top-0 start-100 translate-middle" style={{transform: 'translate(30%, -50%)'}}>1</span>
+        <header className="header">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ height: '80px' }}>
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={logotype} alt="Your Brand" style={{ height: '40px', width: '180px' }} />
                     </a>
-                    <div className="dropdown">
-                        <a className="nav-link d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src={maguire} alt="Profile" className="rounded-circle" style={{width: '60px', height: '60px'}} />
-                            <span className="badge bg-danger position-relative top-0 start-0 translate-middle">1</span>
-                        </a>
-                        <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a className="dropdown-item" href="/profile">Profile</a></li>
-                            <li><a className="dropdown-item" href="/settings">Settings</a></li>
-                            <li><a className="dropdown-item" href="/notifications">Notifications</a></li>
-                            <li><hr className="dropdown-divider" /></li>
-                            <li><a className="dropdown-item" href="/logout">Logout</a></li>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Dashboard</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/team">Team</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/projects">Projects</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">Login</a>
+                            </li>
                         </ul>
+                        <div className="d-flex align-items-center">
+
+                            <div className="form-check form-switch"> 
+                                <input className="form-check-input" type="checkbox" id="darkModeSwitch" />
+                                <label className="form-check-label" htmlFor="darkModeSwitch">
+                                    <i className="fas fa-sun"></i>
+                                    <i className="fas fa-moon"></i>
+                                </label>
+                            </div>
+
+                            <a className="nav-link position-relative" href="/cart">
+                                <i className="fas fa-shopping-cart fa-lg"></i>
+                                <span className="badge bg-warning position-absolute top-0 start-90 translate-middle">1</span>
+                            </a>
+                            <div className="dropdown">
+                                <a className="nav-link d-flex align-items-center" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    <img src={maguire} alt="Profile" className="rounded-circle" style={{ width: '60px', height: '60px' }} />
+                                    <span className="badge bg-warning position-relative top-0 start-0 translate-middle">18</span>
+                                </a>
+                                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="/profile">Profile</a></li>
+                                    <li><a className="dropdown-item" href="/settings">Settings</a></li>
+                                    <li><a className="dropdown-item" href="/notifications">Notifications</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="/logout">Logout</a></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </nav>
-</header>
+            </nav>
+        </header >
 
 
 
