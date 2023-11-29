65f6f79be398e4ba419bb378150f031117f0486d
Kentsukaii Tenente <dvtenente@protonmail.com>
2023-11-29 21:49:50 +0000

    address button is not working

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,23 +1,28 @@
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
-import { MDBInput, MDBBtn } from 'mdb-react-ui-kit';
+import { MDBInput, MDBBtn, MDBModal, MDBModalDialog, MDBModalContent, MDBModalHeader, MDBModalBody, MDBModalFooter } from 'mdb-react-ui-kit';
 
 const Profile = () => {
   const auth = getAuth();
   const [user, setUser] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setUser(user);
     });
 
-    // Limpar a assinatura quando o componente é desmontado
+    // Cleanup subscription when the component is unmounted
     return () => unsubscribe();
   }, [auth]);
 
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
+  };
+
+  const toggleShowModal = () => {
+    setIsOpen(!isOpen);
   };
 
   return (
@@ -51,7 +56,7 @@ const Profile = () => {
                   <MDBInput label='Username' id='form1' type='text' />
                 </div>
                 <div className="col-md-6">
-                  <MDBInput label='Password' id='form2' type={showPassword ? 'text' : 'password'} togglePassword={() => <MDBIcon icon={showPassword ? 'eye-slash' : 'eye'} onClick={toggleShowPassword} />} />
+                  <MDBInput label='Password' id='form2' type={showPassword ? 'text' : 'password'} />
                 </div>
               </div>
               <div className="row mb-4">
@@ -69,15 +74,15 @@ const Profile = () => {
                 <div className="row mb-4">
                 <p>Change Password</p>
                   <div className="col-md-6">
-                    <MDBInput label='Current Password' id='form5' type={showPassword ? 'text' : 'password'} togglePassword={() => <MDBIcon icon={showPassword ? 'eye-slash' : 'eye'} onClick={toggleShowPassword} />} />
+                    <MDBInput label='Current Password' id='form5' type={showPassword ? 'text' : 'password'} />
                   </div>
                   <div className="col-md-6">
-                    <MDBInput label='New Password' id='form6' type={showPassword ? 'text' : 'password'} togglePassword={() => <MDBIcon icon={showPassword ? 'eye-slash' : 'eye'} onClick={toggleShowPassword} />} />
+                    <MDBInput label='New Password' id='form6' type={showPassword ? 'text' : 'password'} />
                   </div>
                 </div>
                 <div className="row mb-4">
                   <div className="col-md-6">
-                    <MDBInput label='Confirm New Password' id='form7' type={showPassword ? 'text' : 'password'} togglePassword={() => <MDBIcon icon={showPassword ? 'eye-slash' : 'eye'} onClick={toggleShowPassword} />} />
+                    <MDBInput label='Confirm New Password' id='form7' type={showPassword ? 'text' : 'password'} />
                   </div>
                 </div>
                 <MDBBtn className='mt-3'>Save</MDBBtn>
@@ -86,6 +91,51 @@ const Profile = () => {
             <div className="p-5" style={{backgroundColor: 'lightpink', marginTop: '20px'}}>
               <h2>Sub Container 3</h2>
               <p>This is the third sub-container.</p>
+              <MDBBtn onClick={toggleShowModal}>Open Form</MDBBtn>
+              <MDBModal tabIndex='-1' show={isOpen} onHide={toggleShowModal}>
+                <MDBModalDialog centered>
+                  <MDBModalContent>
+                    <MDBModalHeader>
+                      <h5 className='modal-title'>New Form</h5>
+                      <MDBBtn className='btn-close' color='none' onClick={toggleShowModal}></MDBBtn>
+                    </MDBModalHeader>
+                    <MDBModalBody>
+                      <form>
+                        <div className="row">
+                          {Array.from({ length: 7 }).map((_, i) => (
+                            <div className="col" key={i}>
+                              <MDBInput label={`Field ${i + 1}`} id={`form${i + 8}`} type='text' />
+                            </div>
+                          ))}
+                        </div>
+                        <div className="row">
+                          <div className="col">
+                            <div className="form-check">
+                              <input className="form-check-input" type="checkbox" id="checkbox1" />
+                              <label className="form-check-label" htmlFor="checkbox1">
+                                Checkbox 1
+                              </label>
+                            </div>
+                          </div>
+                          <div className="col">
+                            <div className="form-check">
+                              <input className="form-check-input" type="checkbox" id="checkbox2" />
+                              <label className="form-check-label" htmlFor="checkbox2">
+                                Checkbox 2
+                              </label>
+                            </div>
+                          </div>
+                        </div>
+                        <MDBBtn className='mt-3'>Submit</MDBBtn>
+                      </form>
+                    </MDBModalBody>
+                    <MDBModalFooter>
+                      <MDBBtn color='secondary' onClick={toggleShowModal}>Close</MDBBtn>
+                      <MDBBtn>Save changes</MDBBtn>
+                    </MDBModalFooter>
+                  </MDBModalContent>
+                </MDBModalDialog>
+              </MDBModal>
             </div>
           </div>
         </div>
