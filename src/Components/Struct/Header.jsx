b0efbd0bf815bb3f34098a5c55110a833261b628
Kentsukaii Tenente <dvtenente@protonmail.com>
2023-12-01 00:03:04 +0000

    New improvised logo

--- src/Components/Struct/Header.jsx
+++ src/Components/Struct/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
             <nav className="navbar navbar-expand-lg navbar-light bg-light" style={{ height: '80px' }}>
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <img src={logotype} alt="Your Brand" style={{ height: '40px', width: '180px' }} />
+                        <img src={logotype} alt="Your Brand" style={{ height: '70px', width: '180px' }} />
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
