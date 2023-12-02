7714d5edf208c60171848e421cc9a8e3e9c93894
pialo3434 <pccosta2001@gmail.com>
2023-12-02 00:56:20 +0000

    facebook e google

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   MDBInput,
   MDBBtn,
@@ -11,6 +11,7 @@ import {
   MDBCheckbox,
 } from "mdb-react-ui-kit";
 import { useFirebaseRegister } from "./Backend";
+
 
 const Register = () => {
   const {
@@ -34,49 +35,27 @@ const Register = () => {
               <h2>Registration is easy and free!</h2>
               <div className="col-md-6">
                 <p></p>
-                <MDBInput
-                  label="E-Mail"
-                  id="form1"
-                  type="text"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
+                <MDBInput label="E-Mail" id="form1" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
               </div>
               <p></p>
               <div className="col-md-6">
-                <MDBInput
-                  label="Password"
-                  id="form2"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
+              <MDBInput label="Password" id="form2" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
               </div>
               <p></p>
               <div className="col-md-6">
-                <MDBInput
-                  label="Password Confirm"
-                  id="form3"
-                  type="password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                />
+              <MDBInput label="Password Confirm" id="form3" type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
               </div>
               <p></p>
-              <p>
-                The password must meet 3 of the following requirements:
-              </p>
+              <p>The password must meet 3 of the following requirements:</p>
               <p>Lowercase character</p>
               <p>Capital character</p>
               <p>Special character</p>
               <p>Number</p>
-              <MDBCheckbox
-                label="I would like to receive personalized news and commercial communications from IntecnoPT via email and other means."
-              />
+              <MDBCheckbox label="I would like to receive personalized news and commercial communications from IntecnoPT via email and other means." />
               <p></p>
               <MDBCheckbox label="I agree to the Terms and Conditions" />
               <p></p>
-              <MDBBtn>Register</MDBBtn>
+              <MDBBtn onClick={handleRegister}>Register</MDBBtn>
             </div>
           </div>
         </div>
@@ -84,18 +63,16 @@ const Register = () => {
           <div className="bg-light p-5">
             <div className="container">
               <h2>Or sign in with a social network</h2>
-              <MDBBtn>
-                Sign In with Facebook
-              </MDBBtn>
-              <MDBBtn>Sign In with Google</MDBBtn>
+              <MDBBtn onClick={handleFacebookRegister}>Sign In with Facebook</MDBBtn>
+              <MDBBtn onClick={handleGoogleRegister}>Sign In with Google</MDBBtn>
               <p></p>
-              <p>Only used for authentication.</p>
+              <h7>Only used for authentication.</h7>
               <p></p>
-              <p>We will never post anything on your profile</p>
+              <h7>We will never post anything on your profile</h7>
               <p></p>
               <h2>Do you already have an IntecnoPT account?</h2>
               <p>Click below to Login!</p>
-              <MDBBtn>Login</MDBBtn>
+              <MDBBtn onClick={''}>Login</MDBBtn>
             </div>
           </div>
         </div>
