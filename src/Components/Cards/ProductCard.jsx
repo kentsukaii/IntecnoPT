72f08d312abf05d8890b2b9b4ee8b150921fd889
pialo3434 <pccosta2001@gmail.com>
2023-12-09 03:39:40 +0000

    wewqwreq2w3rr

--- src/Components/Cards/ProductCard.jsx
+++ src/Components/Cards/ProductCard.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBBtn } from 'mdbreact';
+import { FaCheckCircle, FaStar } from 'react-icons/fa'; // Importing icons
 
-const App = () => {
+const ProductCard = () => {
   return (
-<MDBCard className="p-3 border rounded" style={{ width: "22rem", marginTop: "1rem", backgroundColor: "#f8f9fa" }}>
-      <MDBCardImage className="img-fluid" src="https://via.placeholder.com/400" waves />
-      <MDBCardBody>
-        <MDBCardTitle>Product Title</MDBCardTitle>
-        <MDBCardText>Some quick example text to build on the card title and make up the bulk of the card's content.</MDBCardText>
-        <MDBBtn href="#">MDBBtn</MDBBtn>
-      </MDBCardBody>
-    </MDBCard>
+    <div className="card p-3 border rounded text-left" style={{ flex: '0 0 22rem', marginTop: "1rem", backgroundColor: "#f8f9fa", display: 'inline-block' }}>
+      <img className="card-img-top" src="https://via.placeholder.com/350" alt="Card cap" style={{ width: "100%" }} />
+      <div className="card-body">
+        <h3 className="card-title font-weight-bold">Product Title</h3>
+        <p className="card-text text-muted">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <p className="text-success mb-0"><FaCheckCircle /> Available</p>
+        <p className="text-success" style={{ fontSize: "0.8rem" }}>Expected delivery date: MM/DD/YYYY</p>
+        <h3 className="d-inline-block mr-2 font-weight-bold">00,00 €</h3>
+        <s className="text-muted">00,00 €</s>
+        <div className="mt-4">
+          <button className="btn text" style={{ backgroundColor: "transparent", color: "#4eadfe", fontSize: "0.8rem" }}><FaStar /> Bookmark</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default ProductCard;
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -58,13 +58,13 @@ const Profile = () => {
         displayName: `${firstName} ${lastName}`,
         email: email,
       });
-  
+
       console.log('Firebase Auth profile updated successfully');
-  
+
       // Update the user's document in Firestore
       const userDocRef = doc(firestore, 'Users', auth.currentUser.uid);
       const userDocSnap = await getDoc(userDocRef);
-  
+
       if (userDocSnap.exists()) {
         // If the document exists, update it
         await updateDoc(userDocRef, {
@@ -72,7 +72,7 @@ const Profile = () => {
           Email: email,
           DateOfBirth: dateOfBirth,
         });
-  
+
         console.log('Firestore document updated successfully');
       } else {
         // If the document does not exist, create it
@@ -81,184 +81,121 @@ const Profile = () => {
           Email: email,
           DateOfBirth: dateOfBirth,
         });
-  
+
         console.log('Firestore document created successfully');
       }
-  
+
       console.log('User profile updated successfully');
     } catch (error) {
       console.error('Error updating user profile:', error);
     }
   };
-  
 
-const toggleShowPassword = () => {
-  setShowPassword(!showPassword);
-};
 
-const toggleShowModal = () => {
-  setIsOpen(!isOpen);
-};
+  const toggleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
 
-return (
-  <div className="container-fluid mt-5">
-    <div className="row">
-      <div className="col-md-12">
-        <div className="bg-light p-5">
-          <h1>Hello! - {user ? user.displayName : 'User'}</h1>
-        </div>
+  const toggleShowModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+
+<div className="container-fluid mt-5">
+  <div className="row">
+
+    <div className="col-md-4 mt-5">
+      <div className="bg-light p-5" style={{ backgroundColor: 'lightgray', marginBottom: '20px' }}>
+        <h2>Left Container</h2>
+        <p>This is the left container without subcontainers.</p>
       </div>
-      <div className="col-md-4 mt-5">
-        <div className="bg-light p-5">
-          <div className="container">
-            <div className="p-5" style={{ backgroundColor: 'lightyellow', marginTop: '20px' }}>
-              <h2>Sub Container 1</h2>
-              <p>This is the first sub-container.</p>
+      <div className="bg-light p-5" style={{ backgroundColor: 'lightgray', marginBottom: '20px' }}>
+        <h2>Left Container</h2>
+        <p>This is the left container without subcontainers.</p>
+      </div>
+    </div>
+    <div className="col-md-8 mt-5">
+      <div className="bg-light p-5" style={{ backgroundColor: 'lightgray', marginBottom: '20px' }}>
+        <h2>New Container</h2>
+        <p>This is the new container on top of the right container.</p>
+      </div>
+      <div className="bg-light p-5">
+        <div className="container">
+          <div className="row mb-4">
+            <p>My Account Information</p>
+            <div className="col-md-6">
+              <MDBInput
+                label='First Name'
+                id='form1'
+                type='text'
+                value={user && user.additionalData ? user.additionalData.firstName : ''}
+                onChange={(e) => setUser({ ...user, additionalData: { ...user.additionalData, firstName: e.target.value } })}
+              />
             </div>
-            <div className="p-5" style={{ backgroundColor: 'lightgray', marginTop: '20px' }}>
-              <h2>Sub Container 2</h2>
-              <p>This is the second sub-container.</p>
+            <div className="col-md-6">
+              <MDBInput
+                label='Last Name'
+                id='form1'
+                type='text'
+                value={user && user.additionalData ? user.additionalData.lastName : ''}
+                onChange={(e) => setUser({ ...user, additionalData: { ...user.additionalData, lastName: e.target.value } })}
+              />
             </div>
           </div>
+          <div className="row mb-4">
+            <div className="col-md-6">
+              <MDBInput
+                label='Email'
+                id='form3'
+                type='email'
+                value={user ? user.email : ''}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+              />
+            </div>
+            <div className="col-md-6">
+              <MDBInput
+                label='Date of Birth'
+                id='form4'
+                type='date'
+                value={dateOfBirth}
+                onChange={async (e) => {
+                  const newDateOfBirth = e.target.value;
+                  setDateOfBirth(newDateOfBirth);
+
+                  // Update dateOfBirth in Firestore
+                  const userDocRef = doc(usersCollection, user.uid);
+                  await updateDoc(userDocRef, {
+                    dateofbirth: newDateOfBirth
+                  });
+                }}
+              />
+            </div>
+          </div>
+          <MDBBtn className='mt-3' onClick={handleSave}>Save</MDBBtn>
         </div>
-      </div>
-      <div className="col-md-8 mt-5">
         <div className="bg-light p-5">
           <div className="container">
             <div className="row mb-4">
-              <p>My Account Information</p>
-
-
-
-
+              <p>Change Password</p>
               <div className="col-md-6">
-                <MDBInput
-                  label='First Name'
-                  id='form1'
-                  type='text'
-                  value={user && user.additionalData ? user.additionalData.firstName : ''}
-                  onChange={(e) => setUser({ ...user, additionalData: { ...user.additionalData, firstName: e.target.value } })}
-                />
+                <MDBInput label='Current Password' id='form5' type={showPassword ? 'text' : 'password'} />
               </div>
               <div className="col-md-6">
-                <MDBInput
-                  label='Last Name'
-                  id='form1'
-                  type='text'
-                  value={user && user.additionalData ? user.additionalData.lastName : ''}
-                  onChange={(e) => setUser({ ...user, additionalData: { ...user.additionalData, lastName: e.target.value } })}
-                />
+                <MDBInput label='New Password' id='form6' type={showPassword ? 'text' : 'password'} />
               </div>
             </div>
-            <div className="row mb-4">
-              <div className="col-md-6">
-                <MDBInput
-                  label='Email'
-                  id='form3'
-                  type='email'
-                  value={user ? user.email : ''}
-                  onChange={(e) => setUser({ ...user, email: e.target.value })}
-                />
-              </div>
-              <div className="col-md-6">
-                <MDBInput
-                  label='Date of Birth'
-                  id='form4'
-                  type='date'
-                  value={dateOfBirth}
-                  onChange={async (e) => {
-                    const newDateOfBirth = e.target.value;
-                    setDateOfBirth(newDateOfBirth);
-
-                    // Update dateOfBirth in Firestore
-                    const userDocRef = doc(usersCollection, user.uid);
-                    await updateDoc(userDocRef, {
-                      dateofbirth: newDateOfBirth
-                    });
-                  }}
-                />
-              </div>
-
-
-
-
-
-
-            </div>
-            <MDBBtn className='mt-3' onClick={handleSave}>Save</MDBBtn>
-          </div>
-          <div className="bg-light p-5">
-            <div className="container">
-              <div className="row mb-4">
-                <p>Change Password</p>
-                <div className="col-md-6">
-                  <MDBInput label='Current Password' id='form5' type={showPassword ? 'text' : 'password'} />
-                </div>
-                <div className="col-md-6">
-                  <MDBInput label='New Password' id='form6' type={showPassword ? 'text' : 'password'} />
-                </div>
-              </div>
-              <div className="row mb-4">
-                <div className="col-md-6">
-                  <MDBInput label='Confirm New Password' id='form7' type={showPassword ? 'text' : 'password'} />
-                </div>
-              </div>
-              <MDBBtn className='mt-3'>Save</MDBBtn>
-            </div>
-          </div>
-          <div className="p-5" style={{ backgroundColor: 'lightpink', marginTop: '20px' }}>
-            <h2>Sub Container 3</h2>
-            <p>This is the third sub-container.</p>
-            <MDBBtn onClick={toggleShowModal}>Open Form</MDBBtn>
-            <MDBModal tabIndex='-1' show={isOpen} onHide={toggleShowModal}>
-              <MDBModalDialog centered>
-                <MDBModalContent>
-                  <MDBModalHeader>
-                    <h5 className='modal-title'>New Form</h5>
-                    <MDBBtn className='btn-close' color='none' onClick={toggleShowModal}></MDBBtn>
-                  </MDBModalHeader>
-                  <MDBModalBody>
-                    <form>
-                      <div className="row">
-                        {Array.from({ length: 7 }).map((_, i) => (
-                          <div className="col" key={i}>
-                            <MDBInput label={`Field ${i + 1}`} id={`form${i + 8}`} type='text' />
-                          </div>
-                        ))}
-                      </div>
-                      <div className="row">
-                        <div className="col">
-                          <div className="form-check">
-                            <input className="form-check-input" type="checkbox" id="checkbox1" />
-                            <label className="form-check-label" htmlFor="checkbox1">
-                              Checkbox 1
-                            </label>
-                          </div>
-                        </div>
-                        <div className="col">
-                          <div className="form-check">
-                            <input className="form-check-input" type="checkbox" id="checkbox2" />
-                            <label className="form-check-label" htmlFor="checkbox2">
-                              Checkbox 2
-                            </label>
-                          </div>
-                        </div>
-                      </div>
-                      <MDBBtn className='mt-3'>Submit</MDBBtn>
-                    </form>
-                  </MDBModalBody>
-                  <MDBModalFooter>
-                    <MDBBtn color='secondary' onClick={toggleShowModal}>Close</MDBBtn>
-                    <MDBBtn>Save changes</MDBBtn>
-                  </MDBModalFooter>
-                </MDBModalContent>
-              </MDBModalDialog>
-            </MDBModal>
+            <MDBBtn className='mt-3' >Change Password</MDBBtn>
           </div>
         </div>
       </div>
     </div>
   </div>
-);
+</div>
+
+
+
+  
+  );
 };
 export default Profile;
