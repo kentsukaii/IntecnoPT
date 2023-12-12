22e411cce1355d8eea3f471043cd31b72c0d0ce5
pialo3434 <pccosta2001@gmail.com>
2023-12-12 20:04:38 +0000

    finalmente adeus trabalho de merda

--- src/Pages/Steps/StepThree.jsx
+++ src/Pages/Steps/StepThree.jsx
@@ -1,23 +1,107 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Card, Modal } from 'react-bootstrap';
+import { saveAs } from 'file-saver';
+import { useNavigate } from 'react-router-dom';
+import Spinner from 'react-bootstrap/Spinner';
+import { doc, getDoc } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { firestore } from '../../firebase';
 
 function StepThree({ prevStep, formData }) {
   const { fullName, address, region, postalCode, country, phoneNumber, paymentMethod } = formData;
+  const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+
+  const handleConfirmPurchase = async () => {
+    setLoading(true);
+  
+    const delay = Math.random() * 5000;
+    await new Promise(resolve => setTimeout(resolve, delay));
+  
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const cartRef = doc(firestore, 'Cart', user.uid);
+    const cartSnap = await getDoc(cartRef);
+    const productsList = cartSnap.data().productsList;
+  
+    const productDetails = [];
+    let totalPrice = 0;
+    for (const productId of productsList) {
+      const productRef = doc(firestore, 'Products', productId);
+      const productSnap = await getDoc(productRef);
+      const productData = productSnap.data();
+      productDetails.push(productData);
+      totalPrice += productData.Price; // assuming Price field contains the price of the product
+    }
+  
+    let invoice = `
+      Full Name: ${fullName}
+      Address: ${address}
+      Region: ${region}
+      Postal Code: ${postalCode}
+      Country: ${country}
+      Phone Number: ${phoneNumber}
+      Payment Method: ${paymentMethod}
+      Number of Products: ${productsList.length}
+      Total Price: ${totalPrice}
+      Products:
+    `;
+    productDetails.forEach((product, index) => {
+      invoice += `
+        Product ${index + 1}:
+        Name: ${product.Name}
+        Price: ${product.Price}
+        Category: ${product.Category}
+        Expected delivery date: ${product.ExpectedDeliveryDate}
+      `;
+    });
+    const blob = new Blob([invoice], {type: "text/plain;charset=utf-8"});
+    saveAs(blob, "invoice.txt");
+  
+    setShowModal(true);
+    setLoading(false);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+    navigate('/');
+  };
+  
 
   return (
-    <>
-      <h3>Review Your Order</h3>
-      <p><strong>Full Name:</strong> {fullName}</p>
-      <p><strong>Address:</strong> {address}</p>
-      <p><strong>Region:</strong> {region}</p>
-      <p><strong>Postal Code:</strong> {postalCode}</p>
-      <p><strong>Country:</strong> {country}</p>
-      <p><strong>Phone Number:</strong> {phoneNumber}</p>
-      <p><strong>Payment Method:</strong> {paymentMethod}</p>
+    <div className="mt-4" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+      <Card style={{ width: '18rem' }}>
+        <Card.Body>
+          <Card.Title>Review Your Order</Card.Title>
+          <Card.Text>
+            <strong>Full Name:</strong> {fullName}<br/>
+            <strong>Address:</strong> {address}<br/>
+            <strong>Region:</strong> {region}<br/>
+            <strong>Postal Code:</strong> {postalCode}<br/>
+            <strong>Country:</strong> {country}<br/>
+            <strong>Phone Number:</strong> {phoneNumber}<br/>
+            <strong>Payment Method:</strong> {paymentMethod}<br/>
+          </Card.Text>
+          <Button variant="secondary" onClick={prevStep}>Back</Button>
+          <Button variant="success" onClick={handleConfirmPurchase} disabled={loading}>
+            {loading ? <Spinner animation="border" /> : 'Confirm Purchase'}
+          </Button>
+        </Card.Body>
+      </Card>
 
-      <Button variant="secondary" onClick={prevStep}>Back</Button>
-      <Button variant="success">Confirm Purchase</Button>
-    </>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Success</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Your purchase was successful! You will be redirected to the homepage.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
   );
 }
 
